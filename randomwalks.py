09e5bfb55dee662c5fa8ea7605136e815af6e16b
JoepClaes <101040472+JoepClaes@users.noreply.github.com>
2022-03-14 14:49:32 +0100

    Oplossen van differentiaal vergelijking en overal probability check toegevoegd

--- randomwalks.py
+++ randomwalks.py
@@ -5,8 +5,6 @@ import matplotlib.pyplot as plt
 
 n=100   #number of steps in walk
 
-pos_wall = -30
-
 #initial state: (up_initial*|up> + down_initial*|down>) tensor |pos0>
 pos0 = 0    #initial position
 up_initial = 1/math.sqrt(2)
@@ -19,6 +17,7 @@ positions = np.arange(-n+pos0,n+1+pos0,2)
 c_up = np.zeros(2*n+1,dtype=complex)
 c_down = np.zeros(2*n+1,dtype=complex)
 
+#plots the probability distribution of a QRW based on initial conditions
 def qrw(n):
     c_up[n] = up_initial
     c_down[n] = down_initial
@@ -42,11 +41,31 @@ def qrw(n):
         prob_tot[i] = (prob_up[i] + prob_down[i])/(2**n)
     return prob_tot
 
+distr = qrw(n)
+check = 0
+for i in range(2*n):
+    check = check+distr[i]
+
+print(check)
+plt.xlabel("position")
+plt.ylabel("probability)
+plt.plot(positions,distr[::2])
+
+pos_wall = 20
+
+positions = np.arange(-n+pos0,n+1+pos0,2)
+
+c_up = np.zeros(2*n+1,dtype=complex)
+c_down = np.zeros(2*n+1,dtype=complex)
+
+#plots the probability disctribution of a QRW with an absorbing wall at pos_wall
 def absorption(n,pos_wall):
+    c_up = np.zeros(2*n+1,dtype=complex)
+    c_down = np.zeros(2*n+1,dtype=complex)
     c_up[n] = up_initial
     c_down[n] = down_initial
     for i in range(n):
-        print(c_up,c_down)
+        #print(c_up,c_down)
         c_up_new = np.zeros(len(c_up),dtype=complex)
         c_down_new = np.zeros(len(c_down),dtype=complex)
         for k in range(2*n):
@@ -67,6 +86,26 @@ def absorption(n,pos_wall):
         prob_tot[i] = (prob_up[i] + prob_down[i])/(2**n)
     return prob_tot
 
+
+distr = absorption(n,pos_wall)
+check = 0
+for i in range(len(distr)):
+    check = check+distr[i]
+
+print(check)
+plt.xlabel("position")
+plt.ylabel("probability)
+plt.plot(positions,distr[::2])
+
+pos_wall = 20
+
+
+positions = np.arange(-n+pos0,n+1+pos0,2)
+
+c_up = np.zeros(2*n+1,dtype=complex)
+c_down = np.zeros(2*n+1,dtype=complex)
+
+#plots the probability disctribution of a QRW with a reflecting wall at pos_wall
 def reflection(n,pos_wall):
     c_up[n] = up_initial
     c_down[n] = down_initial
@@ -102,31 +141,115 @@ def reflection(n,pos_wall):
         prob_tot[i] = (prob_up[i] + prob_down[i])/(2**n)
     return prob_tot
 
-d = 2
+distr = reflection(n,pos_wall)
+check = 0
+for i in range(len(distr)):
+    check = check+distr[i]
+
+print(check)
+plt.xlabel("position")
+plt.ylabel("probability)
+plt.plot(positions,distr[::2])
+
+d = 5
 jumprate = 1
-k = 1
 
 #make Mab matrix for formula (2) of Childs
-def bintree(n,jumprate,k):
+def bintree(n,jumprate):
     matrix = np.zeros((2**(n+1)+2**n-2,2**(n+1)+2**n-2))
+    k = np.zeros(2**(n+1)+2**n-2)
     for i in range(0,len(matrix)):
-        matrix[i,i] = jumprate
+        if i == 0:
+            k[i] = 2
+        elif 2**n-1 <= i <= 2**(n+1)-2 :
+            k[i] = 2
+        elif i == 2**(n+1)+2**n-3:
+            k[i] = 2
+        else:
+            k[i] = 3
+        matrix[i,i] = k[i]*jumprate
         if i <= 2**n-2:
-            matrix[i,2*(i+1)-1] = -k*jumprate
-            matrix[i,2*(i+1)] = -k*jumprate
-            matrix[2*(i+1)-1,i] = -k*jumprate
-            matrix[2*(i+1),i] = -k*jumprate
-            matrix[-(i+1),-2*(i+1)-1] = -k*jumprate
-            matrix[-(i+1),-2*(i+1)] = -k*jumprate
-            matrix[-2*(i+1)-1,-(i+1)] = -k*jumprate
-            matrix[-2*(i+1),-(i+1)] = -k*jumprate   
+            matrix[i,2*(i+1)-1] = -jumprate
+            matrix[i,2*(i+1)] = -jumprate
+            matrix[2*(i+1)-1,i] = -jumprate
+            matrix[2*(i+1),i] = -jumprate
+            matrix[-(i+1),-2*(i+1)-1] = -jumprate
+            matrix[-(i+1),-2*(i+1)] = -jumprate
+            matrix[-2*(i+1)-1,-(i+1)] = -jumprate
+            matrix[-2*(i+1),-(i+1)] = -jumprate   
     return matrix
 
-print(bintree(d,jumprate,k))
+
+t = np.linspace(0,10)
+
+p0= np.zeros(2**(n+1)+2**n-2)
+p0[0] = 1                        #starts at left vertex
+             
+M = bintree(d,jumprate)
+
+#solve formula (2) of CHilds
+def partdiff(p,t):
+    dpdt = np.zeros((2**(n+1)+2**n-2))
+    for a in range(len(dpdt)):
+        for b in range(len(dpdt)):
+            dpdt[a] = dpdt[a] - M[a,b]*p[b]
+    return dpdt
+
+dpdt = partdiff(p0,t)
+
+p = odeint(partdiff,p0,t)
+
+vertex = 2
+p_vertex = np.zeros(len(p))
+
+for i in range(len(p_vertex)):
+    p_vertex[i] = p[i,vertex]
+
+#plotting the probability to be in 1 vertex against time
+plt.xlabel("time")
+plt.ylabel("Probability particle is in the vertex")
+plt.plot(t,p_vertex)
+plt.show()
+
+p_t = np.zeros(2**(n+1)+2**n-2)
+vertices = np.arange(0,2**(n+1)+2**n-2)
+
+for i in range(len(p_t)):
+    for x in range(len(p_vertex)):
+        p_vertex[x] = p[x,i]
+    p_t[i] = p_vertex[-1]
+
+print(p_t)
+plt.xlabel("vertex")
+plt.ylabel("Probability particle is in the vertex")
+plt.plot(vertices,p_t)
+plt.show()
+
+#adding the values for all vertices in the same columns
+p_column = np.zeros(2*n+1)
+columns = np.arange(0,2*n+1)
+for i in range(len(p_t)):
+    for x in range(len(p_t)+1):
+        if x <= 2**(n+1)-1:
+            if 2**i <= x <= 2**(i+1)-1:
+                p_column[i] = p_column[i] + p_t[x-1]
+        if x >= 2**(n+1)-1:
+            if (2**(n+1)+2**n-1)-(2**(i+1)-1) <= x <= (2**(n+1)+2**n-1)-(2**i):
+                p_column[-i-1] = p_column[-i-1] + p_t[x-1]
+
+print(p_column)
+check = sum(p_column)
+print(check)
+
+plt.xlabel("column")
+plt.ylabel("Probability particle is in the column")
+plt.plot(columns,p_column)
+plt.show()
 
 d = 6
 gamma = 1
 
+#creates the matrix to reduce the binary tree problem to a problem on a line for each column
 def reductionmatrix(n,gamma):
     matrix = np.zeros((2*n+1,2*n+1))
     for i in range(2*n+1):
@@ -148,17 +271,4 @@ def reductionmatrix(n,gamma):
 
 print(reductionmatrix(d,gamma))
 
-distr = qrw(n)
-ptot = 0
-for i in range(2*n):
-    ptot = ptot+distr[i]
 
-print(ptot)
-plt.plot(positions,distr[::2])
-print(len(positions),len(distr[::2]))
-
-plt.plot(positions,absorption(n,pos_wall)[::2])
-print(len(positions),len(absorption(n,pos_wall)[::2]))
-
-plt.plot(positions,reflection(n,pos_wall)[::2])
-print(len(positions),len(reflection(n,pos_wall)[::2]))

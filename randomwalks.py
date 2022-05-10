84cc6e52b65c5a22b39fb9c64654bee62244e0c9
JoepClaes <101040472+JoepClaes@users.noreply.github.com>
2022-05-10 17:01:19 +0200

    Update randomwalks.py
    Implemented the working solve_ivp to solve the DE and added the random walks for a hypercube

--- randomwalks.py
+++ randomwalks.py
@@ -1,4 +1,4 @@
-from scipy.integrate import odeint
+from scipy.integrate import solve_ivp
 import math
 import numpy as np
 import matplotlib.pyplot as plt
@@ -151,13 +151,13 @@ plt.xlabel("position")
 plt.ylabel("probability)
 plt.plot(positions,distr[::2])
 
-d = 5
-jumprate = 1
+n = 2
 
+jumprate = 1
 #make Mab matrix for formula (2) of Childs
 def bintree(n,jumprate):
-    matrix = np.zeros((2**(n+1)+2**n-2,2**(n+1)+2**n-2))
-    k = np.zeros(2**(n+1)+2**n-2)
+    matrix = np.zeros((2**(n+1)+2**n-2,2**(n+1)+2**n-2),dtype=complex)
+    k = np.zeros(2**(n+1)+2**n-2,dtype=complex)
     for i in range(0,len(matrix)):
         if i == 0:
             k[i] = 2
@@ -180,44 +180,52 @@ def bintree(n,jumprate):
     return matrix
 
 
-t = np.linspace(0,10)
+t = np.linspace(0,20,100)
 
-p0= np.zeros(2**(n+1)+2**n-2)
-p0[0] = 1                        #starts at left vertex
-             
-M = bintree(d,jumprate)
+p0= np.zeros(2**(n+1)+2**n-2,dtype=complex)
+p0[0] = 1/np.sqrt(2)
+p0[-1] = 1/np.sqrt(2)                       #starts at left vertex
+print(p0)        
+M = bintree(n,jumprate)
 
-#solve formula (2) of CHilds
-def partdiff(p,t):
-    dpdt = np.zeros((2**(n+1)+2**n-2))
+
+#solve formula (2) or (3) of Childs
+def partdiff(t,p):
+    dpdt = np.zeros((2**(n+1)+2**n-2),dtype=complex)
     for a in range(len(dpdt)):
         for b in range(len(dpdt)):
-            dpdt[a] = dpdt[a] - M[a,b]*p[b]
+            dpdt[a] = dpdt[a] + M[a,b]*p[b]*1j
     return dpdt
 
-dpdt = partdiff(p0,t)
+dp_dt = partdiff((t[0],t[-1]),p0)
 
-p = odeint(partdiff,p0,t)
-
-vertex = 2
-p_vertex = np.zeros(len(p))
+prob = solve_ivp(partdiff,(t[0],t[-1]),p0,t_eval = t)
+p = np.zeros((len(t),2**(n+1)+2**n-2),dtype=complex)
+for i in range(len(t)):
+    for j in range(2**(n+1)+2**n-2):
+        p[i,j] = abs(prob.y[j,i])**2
+print(p)
+p_new = p
+#which vertex are we looking at
+vertex = 4
+p_vertex = np.zeros(len(p),dtype=complex)
 
 for i in range(len(p_vertex)):
     p_vertex[i] = p[i,vertex]
 
-#plotting the probability to be in 1 vertex against time
+##plotting the probability to be in 1 vertex against time
 plt.xlabel("time")
-plt.ylabel("Probability particle is in the vertex")
+plt.ylabel("Probability particle is in the vertex " + str(vertex))
 plt.plot(t,p_vertex)
 plt.show()
 
-p_t = np.zeros(2**(n+1)+2**n-2)
-vertices = np.arange(0,2**(n+1)+2**n-2)
+p_t = np.zeros(2**(n+1)+2**n-2,dtype=complex)
+vertices = np.arange(0,2**(n+1)+2**n-2,dtype=complex)
 
 for i in range(len(p_t)):
     for x in range(len(p_vertex)):
         p_vertex[x] = p[x,i]
-    p_t[i] = p_vertex[-1]
+    p_t[i] = p_vertex[-3]
 
 print(p_t)
 plt.xlabel("vertex")
@@ -225,8 +233,12 @@ plt.ylabel("Probability particle is in the vertex")
 plt.plot(vertices,p_t)
 plt.show()
 
+print(p_t)
+check = sum(p_t)
+print(check)
+
 #adding the values for all vertices in the same columns
-p_column = np.zeros(2*n+1)
+p_column = np.zeros(2*n+1,dtype=complex)
 columns = np.arange(0,2*n+1)
 for i in range(len(p_t)):
     for x in range(len(p_t)+1):
@@ -245,30 +257,112 @@ plt.xlabel("column")
 plt.ylabel("Probability particle is in the column")
 plt.plot(columns,p_column)
 plt.show()
+           
+           n = 5               #n-hypercube
+vertices = 2**n   #number of vertices
+jumprate = 1
 
-d = 6
-gamma = 1
+def DecToBin(n):
+    return bin(n).replace("0b","")
 
-#creates the matrix to reduce the binary tree problem to a problem on a line for each column
-def reductionmatrix(n,gamma):
-    matrix = np.zeros((2*n+1,2*n+1))
-    for i in range(2*n+1):
-        if i == 0:
-            matrix[i,i] = 2*gamma
-            matrix[i,i+1] = -2*gamma
-        elif i == n:
-            matrix[i,i] = 2*gamma
-            matrix[i,i+1] = -2*gamma
-            matrix[i,i-1] = -2*gamma
-        elif i == 2*n:
-            matrix[i,i] = 2*gamma
-            matrix[i,i-1] = -2*gamma
-        else:
-            matrix[i,i] = 3*gamma
-            matrix[i,i+1] = -2*gamma
-            matrix[i,i-1] = -2*gamma
+def hammingWeight(n):
+      """
+      :type n: int
+      :rtype: int
+      """
+      x = DecToBin(n)
+      print(x)
+      one_count = 0
+      for i in x:
+         if i == "1":
+            one_count+=1
+      return one_count
+
+num = 939
+print(hammingWeight(num))
+
+#make matrix for an n-hypercub
+def hypercube(n,jumprate):
+    matrix = np.zeros((vertices,vertices),dtype=complex)
+    for i in range(0,len(matrix)):
+        for  x in range(n):
+            if i <= 2**(x)-1:
+                matrix[2**(x)+i,i] = -jumprate
+                matrix[i,2**(x)+i] = -jumprate
+                matrix[-2**(x)-(i+1),-(i+1)] = -jumprate
+                matrix[-(i+1),-2**(x)-(i+1)] = -jumprate
+            if 2**(x-1) <= i <=2**x-1:
+                for a in range(2**(x-1),2**x):
+                    matrix[i,a] = matrix[i-2**(x-1),a-2**(x-1)]
+                    matrix[-(i+1),-(a+1)] = matrix[i-2**(x-1),a-2**(x-1)]
     return matrix
 
-print(reductionmatrix(d,gamma))
+M = hypercube(n,jumprate)
+t = np.linspace(0,0.7,1000)
+
+p0= np.zeros(vertices,dtype=complex)
+p0[0] = 1                        #starts at left vertex
+print(p0)
 
 
+#solve formula (2) or (3) of Childs
+def partdiff(t,p):
+    dpdt = np.zeros((vertices),dtype=complex)
+    for a in range(len(dpdt)):
+        for b in range(len(dpdt)):
+            dpdt[a] = dpdt[a] + M[a,b]*p[b]*1j
+    return dpdt
+
+dp_dt = partdiff((t[0],t[-1]),p0)
+
+prob = solve_ivp(partdiff,(t[0],t[-1]),p0,t_eval = t)
+p = np.zeros((len(t),vertices),dtype=complex)
+for i in range(len(t)):
+    for j in range(vertices):
+        p[i,j] = abs(prob.y[j,i])**2
+print(p)
+p_new = p
+#which vertex are we looking at
+vertex = -1
+p_vertex = np.zeros(len(p),dtype=complex)
+
+for i in range(len(p_vertex)):
+    p_vertex[i] = p[i,vertex]
+
+##plotting the probability to be in 1 vertex against time
+plt.xlabel("time")
+plt.ylabel("Probability particle is in the vertex " + str(vertex))
+plt.plot(t,p_vertex)
+plt.show()
+
+p_t = np.zeros(vertices,dtype=complex)
+vertices = np.arange(0,vertices,dtype=complex)
+
+for i in range(len(p_t)):
+    for x in range(len(p_vertex)):
+        p_vertex[x] = p[x,i]
+    p_t[i] = p_vertex[-3]
+
+
+print(p_t)
+plt.xlabel("vertex")
+plt.ylabel("Probability particle is in the vertex")
+plt.plot(vertices,p_t)
+plt.show()
+
+check1 = sum(p_t)
+
+#adding the values for all vertices in the same columns
+p_column = np.zeros(n+1,dtype=complex)
+columns = np.arange(0,n+1)
+for i in range(len(p_t)):
+    p_column[hammingWeight(i)] = p_column[hammingWeight(i)] + p_t[i]
+
+print(p_column)
+check2 = sum(p_column)
+
+plt.xlabel("column")
+plt.ylabel("Probability particle is in the column")
+plt.plot(columns,p_column)
+plt.show()
+           
